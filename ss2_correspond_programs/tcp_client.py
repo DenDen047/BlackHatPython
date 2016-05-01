e2bb1787464ec67b07b9289213f1809c7f2586d2
DenDen047 <sh.mn.nat@gmail.com>
2016-05-01 20:59:01 +0900

    add tcp_client.py

--- ss2_correspond_programs/tcp_client.py
+++ ss2_correspond_programs/tcp_client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9999
 
 # create socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +12,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("ABCDEF\r\n\r\n")
 
 # receive data
 response = client.recv(4096)
